c909a00a0a9e5d9bb45b7f6d18582d27d9361603
yiplee <guoyinl@gmail.com>
2017-12-11 18:32:53 +0800

    添加 demo

--- Example/YPAVAssetResourceLoader/YPViewController.m
+++ Example/YPAVAssetResourceLoader/YPViewController.m
@@ -7,23 +7,41 @@
 //
 
 #import "YPViewController.h"
+#import <YPAVAssetResourceLoader/YPAVPlayerResourceLoader.h>
+#import <AVKit/AVKit.h>
 
 @interface YPViewController ()
+@property (strong, nonatomic) IBOutlet UITextView *textView;
+@property (strong, nonatomic) IBOutlet UISwitch *resourceLoaderSwitch;
 
 @end
 
 @implementation YPViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)playVideo:(id)sender {
+    NSString *urlString = self.textView.text;
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSParameterAssert(url);
+    
+    BOOL useResourceLoader = self.resourceLoaderSwitch.isOn;
+    
+    AVPlayerItem *playerItem = nil;
+    if (useResourceLoader) {
+        AVAsset *asset = [AVURLAsset assetWithYPResourceURL:url];
+        playerItem = [AVPlayerItem playerItemWithAsset:asset];
+    } else {
+        playerItem = [AVPlayerItem playerItemWithURL:url];
+    }
+    
+    AVPlayer *player = [AVPlayer playerWithPlayerItem:playerItem];
+    if (useResourceLoader) {
+        player.automaticallyWaitsToMinimizeStalling = NO;
+    }
+    AVPlayerViewController *playerController = [AVPlayerViewController new];
+    playerController.player = player;
+    [self presentViewController:playerController
+                       animated:YES
+                     completion:nil];
 }
 
 @end
